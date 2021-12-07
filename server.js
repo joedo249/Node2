11bdb6b5c1fbe4f5f38cad9f907ae6b7e2a6d1aa
Joel Dousset <jldc249@gmail.com>
2021-12-07 12:49:36 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-// const _ = require("lodash");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
   const num = _.random(0, 20);
