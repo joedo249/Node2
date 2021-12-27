742fb4320f65259d9961fc651fcc8429e8fd1c72
Joel Dousset <jldc249@gmail.com>
2021-12-27 18:32:35 +0100

    express.static, morgan

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // register view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000);
 
-app.get("/", (req, res) => {
+// middleware and static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
   const blogs = [
     {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
+      title: 'Yoshi finds eggs',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
     },
     {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
+      title: 'Mario finds stars',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
     },
     {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
+      title: 'How to defeat bowser',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
     },
   ];
-  res.render("index", { title: "Home", blogs });
+  res.render('index', { title: 'Home', blogs });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create" });
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create' });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404", { title: "404" });
+  res.status(404).render('404', { title: '404' });
 });
